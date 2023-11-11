4b0022424105a8a5af40e452c5fa647887be7043
Achmad Baihaqi <haqiachd@gmail.com>
2023-11-11 13:54:53 +0700

    add retrofit dependencies

---
+++ app/src/main/java/com/tif22/orderingfood/ui/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.tif22.orderingfood.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tif22.orderingfood.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
