90dcf62a227c9b189f52cdb1bc64d174d4a4a159
Achmad Baihaqi <haqiachd@gmail.com>
2023-11-11 14:03:00 +0700

    add new class for retrofit client and end point

---
+++ app/src/main/java/com/tif22/orderingfood/api/retrofit/RetrofitClient.kt
@@ -0,0 +1,36 @@
+package com.tif22.orderingfood.api.retrofit
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import com.google.gson.GsonBuilder
+
+import java.util.concurrent.TimeUnit
+
+object RetrofitClient {
+
+        const val BASE_URL : String = "http://????" // local server
+
+    /**
+     * connect to rest server
+     */
+    val connection: Retrofit
+        get() {
+            val gson = GsonBuilder().setLenient().create()
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+            val client: OkHttpClient = OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .writeTimeout(60, TimeUnit.SECONDS)
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(client)
+                .build()
+        }
+}
